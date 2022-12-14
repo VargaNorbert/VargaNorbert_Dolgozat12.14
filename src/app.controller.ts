659d5a4f008a96693529f5fa132ebf833b58194f
Varga Norbert <vnorbi02@gmail.com>
2022-12-14 13:52:13 +0100

    kész

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,7 @@
-import {
-  Controller,
-  Get,
-  Render,
-} from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Redirect, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 import db from './db';
+import { MacskaDto } from './macska.dto';
 
 @Controller()
 export class AppController {
@@ -12,7 +9,45 @@ export class AppController {
 
   @Get()
   @Render('index')
-  index() {
-    return { message: 'Welcome to the homepage' };
+  async listMacska() {
+    const [rows] = await db.execute(
+      'SELECT suly,szem_szin FROM macskak ORDER BY suly DESC',
+    );
+
+    return {
+      macskak: rows,
+    };
   }
+
+  @Get('cats/szin')
+  @Render('szin')
+  async listSzemszin(@Query('szem_szin') szin = '') {
+    const [rows] = await db.execute(
+      'SELECT suly,szem_szin FROM macskak WHERE szem_szin LIKE ? ORDER BY suly DESC',
+      [szin],
+    );
+
+    return {
+      macskak: rows,
+    };
+  }
+
+  @Get('cats/new')
+  @Render('new')
+  newCatForm() {
+    return {};
+  }
+
+  @Post('cats/new')
+  @Redirect()
+  async newCat(@Body() macskak: MacskaDto) {
+    const [result]: any = await db.execute(
+      'INSERT INTO macskak (suly,szem_szin) VALUES (?,?)',
+      [macskak.suly,macskak.szem_szin],
+    );
+    return {
+      url: '/',
+    };
+  }
+
 }
---
+++ src/macska.dto.ts
@@ -0,0 +1,4 @@
+export class MacskaDto {
+  suly: number;
+  szem_szin: string;
+}
